002b1eeb1aa7d0379b4811db7d22061767eb3d76
Mickael Marcotte <mmarcott@c3r1p4.42quebec.com>
2023-01-29 01:58:47 -0500

    a

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:00:32 by mmarcott          #+#    #+#             */
-/*   Updated: 2023/01/29 01:16:25 by mmarcott         ###   ########.fr       */
+/*   Updated: 2023/01/29 01:58:00 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,12 @@ void	move_player(t_vars *vars, int xmodifier, int ymodifier)
 	if (!check_list(vars->decors, vars->player.x + xmodifier, vars->player.y
 			+ ymodifier))
 	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, vars->player.x
-				+= xmodifier, vars->player.y += ymodifier);
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->player.img,
+				vars->player.x += xmodifier, vars->player.y += ymodifier);
 	}
 	else
 	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, vars->player.x,
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->player.img, vars->player.x,
 				vars->player.y);
 	}
 }
@@ -88,8 +88,8 @@ int	key_pressed(int keycode, t_vars *vars)
 		exit(0);
 	}
 	else
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, vars->player.x,
-				vars->player.y);
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->player.img,
+				vars->player.x, vars->player.y);
 	return (0);
 }
 
@@ -122,6 +122,24 @@ void	add_end_list(t_decors **head, t_decors *ajout)
 	current->next = ajout;
 }
 
+t_decors	*init_decor(t_vars *vars, char *img, char type)
+{
+	t_decors	*new;
+
+	new = ft_calloc(1, sizeof(t_decors));
+	new->img = mlx_xpm_file_to_image(vars->mlx, img, &new->x, &new->y);
+	new->y = 0;
+	new->x = 0;
+	if (type == 'c' || type == 'w')
+	{
+		new->x = 100;
+		new->y = 100;
+	}
+	new->type = type;
+	new->next = NULL;
+	return (new);
+}
+
 int	main(int argc, char **argv)
 {
 	t_decors	*water;
@@ -131,32 +149,19 @@ int	main(int argc, char **argv)
 	t_playerpos	player;
 	char		*map;
 
-	sand = ft_calloc(1, sizeof(t_decors));
-	water = ft_calloc(1, sizeof(t_decors));
-	chest = ft_calloc(1, sizeof(t_decors));
 	if (argc != 2)
 		return (ft_error("You failed to provide args, -> ./so_long <map>"), 1);
 	map = argv[1];
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 1900, 1000, "Le chien marin");
 	player.img = mlx_xpm_file_to_image(vars.mlx, "sprites/character.xpm",
-			&player.x, &player.y);
-	player.x = 200;
+			&vars.player.x, &vars.player.y);
+	player.x = 300;
 	player.y = 300;
-	vars.img = player.img;
-	sand->img = mlx_xpm_file_to_image(vars.mlx, "sprites/sand.xpm", &sand->x,
-			&sand->y);
-	sand->type = 's';
-	sand->x = 0;
-	sand->y = 0;
-	water->img = mlx_xpm_file_to_image(vars.mlx, "sprites/water.xpm", &water->x,
-			&water->y);
-	water->type = 'w';
-	chest->img = mlx_xpm_file_to_image(vars.mlx, "sprites/chest.xpm", &chest->x,
-			&chest->y);
-	chest->type = 'c';
 	vars.player = player;
-	ft_printf("before checkmap\n");
+	sand = init_decor(&vars, "sprites/sand.xpm", 's');
+	water = init_decor(&vars, "sprites/water.xpm", 'w');
+	chest = init_decor(&vars, "sprites/chest.xpm", 'c');
 	ft_check_map(map, &sand, '1', 's');
 	ft_check_map(map, &water, '0', 'w');
 	ft_check_map(map, &chest, 'C', 'c');
